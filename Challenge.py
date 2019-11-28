a71b578cf4aa073879687b7ec01ed13fa9386314
shirmn <shirmn@gmail.com>
2019-11-28 11:35:47 +0200

    edit file

--- Challenge.py
+++ Challenge.py
@@ -1,12 +1,21 @@
-from collections import Counter
 
 f = open("text.txt", "r")
 if f.mode == 'r':
     contents = f.read().split()
     print(contents)
 
-word_count = Counter(contents)
-print(word_count.most_common(1))
+countDict={}
+
+for word in contents:
+    if word in countDict:
+        countDict[word]=countDict[word]+1
+    else:
+        countDict[word] =1
+
+max= max(countDict, key=countDict.get)
+print("The word " +max + " is the most recurring word in that file")
 
 
 
+
+f.close()
