2b2f48633758fa42b8936f50289f997c44730d73
Barak Merdler <barakmerdler@gmail.com>
2019-11-28 13:45:27 +0200

    broke the last print into 2 prints commands

--- Python-Home-Challenges/app.py
+++ Python-Home-Challenges/app.py
@@ -6,6 +6,5 @@ with open("./Python-Home-Challenges/Challenge_01.txt") as file:
             words[word] += 1
         else:
             words[word] = 1
-    print(
-        f'''Most recurring word in that file: {max(words, key=words.get)}
-And it appears {words[max(words, key=words.get)]} times''')
+    print(f'Most recurring word in that file: {max(words, key=words.get)}')
+    print(f'And it appears {words[max(words, key=words.get)]} times')
