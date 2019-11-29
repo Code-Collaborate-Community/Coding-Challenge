7910f1157db721079ce28d6ba2bdfcac4cfcc624
OmerAmsc <Omeramsc@gmail.com>
2019-11-29 12:28:18 +0200

    upgraded the python code

--- reader.py
+++ reader.py
@@ -1,23 +1,45 @@
 def main():
-    find_word_frequency()
+    while True:
+        try:
+            path = path_input()
+            if path:
+                file = open_file(path)
+            else:
+                file = open_file()
+            break
+        except FileNotFoundError as e:
+            print(f'File not found! \n{e}\nTry again:')
+    top_word, word_appear = max_frequency(fetch_words(file))
+    file.close()
+    print(f'Most frequent word is "{top_word}", '
+          f'and it appears {word_appear} times.')
+
+
+def path_input():
+    answer = input("Insert the file's full path "
+                   "OR press Enter to use the default path:")
+    if answer is None:
+        return
+    return answer
 
 
 def max_frequency(words):
     most_frequent = max(words, key=words.get)
-    print(most_frequent, words[most_frequent])
+    return most_frequent, words[most_frequent]
 
 
-def find_word_frequency():
-    try:
-        with open('./Python-Home-Challenges/Challenge_01.txt', 'r') as file:
-            words = dict()
-            for line in file:
-                for word in line.strip(",.!:\n").split(' '):
-                    if word is not '':
-                        words[word.lower()] = words.get(word, 0) + 1
-            return max_frequency(words)
-    except FileNotFoundError as e:
-        print('File not found')
+def fetch_words(file):
+    words = dict()
+    for line in file:
+        for word in line.strip("!@#$%^&*()_-+=~`,.':;<>\n").split(' '):
+            if word != '':
+                words[word.lower()] = words.get(word, 0) + 1
+    return words
+
+
+def open_file(path='./Python-Home-Challenges/Challenge_01.txt'):
+    return open(path, 'r')
+
 
 if __name__ == '__main__':
     main()
