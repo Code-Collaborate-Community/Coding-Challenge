d5d833fab1b980035e06e746a85693719e2a94bf
Maor Weiss <maorweissb@gmail.com>
2019-12-01 14:14:30 +0200

    Rewrite the 'Python-home-challenge#01-MaorWeiss.py' code

--- Python-Home-Challenges/Python-home-challenge#01-MaorWeiss.py
+++ Python-Home-Challenges/Python-home-challenge#01-MaorWeiss.py
@@ -1,18 +1,16 @@
 # HW03 - Open a text file and returns the most recurring word in that file.
 
-fp = open('/file.txt', 'r')
-# Open file #
-f = fp.readline()
+with open(r'/home/$USER/', 'r') as fp:
+    f = fp.readline()
 # Read file #
 split_words_from_file = f.split()
 
-the_most_recurring_word = ''
+# Creating variable for keeping the recurring value from the file #
 biggest_counting = 0
 
-copy_list = split_words_from_file
-for list_arg in copy_list:
+for list_arg in split_words_from_file:
+    count = 0
     for word in split_words_from_file:
-        count = 0
         if list_arg == word:
             count += 1
     if count > biggest_counting:
@@ -20,6 +18,5 @@ for list_arg in copy_list:
         biggest_counting = count
 
 
-print 'The word "{}" which has appeared {} times is the most recurring word in the file.'.format(the_most_recurring_word, biggest_counting)
-fp.close()
-# Close file #
+print(f"The most recurring word in the file is '{the_most_recurring_word}'")
+print(f"which has appeared {biggest_counting} times.")
