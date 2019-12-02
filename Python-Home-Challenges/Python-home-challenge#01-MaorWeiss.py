d06953754cba16d65cbe8cb58df2f6aa27f1bca7
Maor Weiss <maorweissb@gmail.com>
2019-12-02 20:33:22 +0200

    Changing py code by instructions #01

--- Python-Home-Challenges/Python-home-challenge#01-MaorWeiss.py
+++ Python-Home-Challenges/Python-home-challenge#01-MaorWeiss.py
@@ -1,22 +1,39 @@
 # HW03 - Open a text file and returns the most recurring word in that file.
 
-with open(r'/home/$USER/', 'r') as fp:
-    f = fp.readline()
-# Read file #
-split_words_from_file = f.split()
 
-# Creating variable for checking what is the recurring value  #
-biggest_counting = 0
-
-for list_arg in split_words_from_file:
-    count = 0
-    for word in split_words_from_file:
-        if list_arg == word:
-            count += 1
-    if count > biggest_counting:
-        the_most_recurring_word = list_arg
-        biggest_counting = count
+def read_file():
+    with open('/home/$USER/, 'r') as tmp_file:
+        execute_file = tmp_file.readline()
+    return execute_file
 
 
-print(f"The most recurring word in the file is '{the_most_recurring_word}'")
-print(f"which has appeared {biggest_counting} times.")
+def spilt_file(file):
+    split_words_from_file = file.split()
+    return split_words_from_file
+
+
+def printing_the_recurring_word(split_words_from_file):
+    biggest_counting = 0
+# Creating variable for keeping the recurring value from the file #
+    words = dict()
+    for list_arg in split_words_from_file:
+        if list_arg in words:
+            words[list_arg] = words[list_arg] + 1
+    else:
+        words[list_arg] = 1
+
+    for key in words:
+        if words[key] > biggest_counting:
+            biggest_counting = words[key]
+            the_most_recurring_word = key
+    print(f"The most recurring word in the file is '{the_most_recurring_word}'")
+    print(f"which has appeared {biggest_counting} times.")
+
+
+def main():
+    my_file = read_file()
+    my_split_file = spilt_file(my_file)
+    printing_the_recurring_word(my_split_file)
+
+
+main()
