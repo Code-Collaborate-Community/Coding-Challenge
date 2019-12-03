441f2132242094d0c7ece4e54e5efaafc1d0a0d8
mor <morbitton1993@gmail.com>
2019-12-04 00:36:16 +0200

    fix homework python

--- Hwpython.py
+++ Hwpython.py
@@ -1,13 +1,13 @@
 #my code:
 
-with open('text.txt') as file:
-    List=file.read().split()
-    wordDict={}
+with open('text.txt', 'r') as file:
+    f_read=file.read().split()
+    word_counter={}
     for word in List:
            if word in wordDict:
-               wordDict[word]+=1
+               word_counter[word]+=1
            else:
-                wordDict[word]=1
+                word_counter[word]=1
 
     print(f' the most recurring word in that file: {max(wordDict, key=wordDict.get)}')
     print(f'Appears {wordDict[max(wordDict,key=wordDict.get)]} times')
