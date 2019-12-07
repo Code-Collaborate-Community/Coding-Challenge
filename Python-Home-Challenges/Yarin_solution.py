8873316e86511a1c585b032f0bb2605aa22f7467
Yarinh55 <yarinh55@gmail.com>
2019-12-07 20:33:59 +0200

    fixed solution after review

--- Python-Home-Challenges/Yarin_solution.py
+++ Python-Home-Challenges/Yarin_solution.py
@@ -1,21 +1,26 @@
 
 def most_common_word(file_name):
-    words_dict={}
+    split_it = split_text(file_name)
+    count_words_get_max(split_it)
+
+
+def split_text(file_name):
     with open(file_name) as file:
-        data_set=file.read()
-        split_it= data_set.split()
-        for word in split_it:
-            if word in words_dict.keys():
-                words_dict[word]+=1
-            else:
-                words_dict.update({word:1})
-        maximum = max(words_dict, key=words_dict.get)
-        print(maximum, words_dict[maximum])
+        data_set = file.read()
+        split_it = data_set.split()
+        return split_it
 
 
+def count_words_get_max(split_words):
+    words_dict = {}
+    for word in split_words:
+        if word in words_dict.keys():
+            words_dict[word] += 1
+        else:
+            words_dict.update({word: 1})
+    maximum = max(words_dict, key=words_dict.get)
+    print(maximum, words_dict[maximum])
 
-if __name__== "__main__":
-    file_name = input("insert file path: ")
-    most_common_word(file_name)
 
-
+if __name__ == "__main__":
+    most_common_word("summary.txt")
